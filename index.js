d5fbf20e877a93aec0bf91ad68bada55f4de58af
Abhisek <abhishekdogra169@gmail.com>
2024-07-31 20:05:43 +0530

    Implemented socket listeners

--- index.js
+++ index.js
@@ -9,7 +9,23 @@ const httpServer = createServer(app);
 const io = new Server(httpServer, { cors: "http://localhost:3000" });
 
 io.on("connection", (socket) => {
-  console.log("server connected");
+    console.log("server connected");
+
+    socket.on("beginPath", (data) => {
+    socket.broadcast.emit("beginPath", data); // broadcast to all clients except the sender
+    });
+
+    socket.on("drawLine", (data) => {
+        socket.broadcast.emit("drawLine", data);
+    });
+
+    socket.on("changeConfig", (data) => {
+        socket.broadcast.emit("changeConfig", data);
+    });
+    
+    socket.on("disconnect", () => {
+    console.log("A user disconnected");
+    });
 });
 
 httpServer.listen(5000);
